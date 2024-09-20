252f34e6c0010414fe317e48f752f09389c42df9
Mitchell Shiell <59712867+MitchellShiell@users.noreply.github.com>
2024-09-20 18:17:34 -0400

    added scoreDev profile

---
+++ conductorScripts/deployments/scoreDev.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# Welcome
+echo -e "\033[1;36m╔════════════════════════════════════╗\033[0m"
+echo -e "\033[1;36m║ Welcome to the scoreDev QuickStart ║\033[0m"
+echo -e "\033[1;36m╚════════════════════════════════════╝\033[0m"
+
+# rs = "Run Script" a simple function to apply permissions and run scripts
+rs() {
+        chmod +x "$1" && "$1"
+    }
+
+# KeycloakDb Setup
+rs /scripts/services/keycloakDbSetup.sh
+
+# SongDb Setup
+rs /scripts/services/songDbSetup.sh
+
+# Song Setup
+rs /scripts/services/songSetup.sh
+
+# Success and Next Steps
+echo -e "\033[1;32mSuccess:\033[0m Arranger is now reachable"
+echo -e "\033[1;36m╔══════════════════════════════════╗\033[0m"
+echo -e "\033[1;36m║ Score Dev Service Setup Complete ║\033[0m"
+echo -e "\033[1;36m╚══════════════════════════════════╝\033[0m"
+echo -e "\033[1m1️⃣  To run Score locally, start by cloning the repo:\033[0m\n"
+echo -e "   \033[1;32mgit clone https://github.com/overture-stack/score.git\033[0m\n"
+echo -e "\033[1m2️⃣  Then install the dependencies by running:\033[0m\n"
+echo -e "   \033[1;32mnpm ci\033[0m\n"
+echo -e "\033[1m3️⃣  Rename \033[1;32m.env.scoreDev\033[0m to \033[1;32m.env:\033[0m\n"
+echo -e "\033[1m4️⃣  Run the development server:\033[0m\n"
+echo -e "   \033[1;32mnpm run dev\033[0m\n"
